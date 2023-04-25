d741ae45a321d2e4956ffe533085005d431922d0
ssambinelli <ssambinelli@icloud.com>
2023-04-25 12:20:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fix this!!! 
+
+function fullName(element){
+    if (element.innerText = "Ss."){
+        element.innerText = 'Sabrina Sambinelli'
+    }
+    if(element.innerText = 'Sabrina Sambinelli'){
+        element.innerText = "Ss."
+    }
+}
