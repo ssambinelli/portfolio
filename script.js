968522f73d9310c57da4bf00dcde0f4d6469c453
ssambinelli <ssambinelli@icloud.com>
2023-04-26 19:37:26 -0400

    toggle button added

--- script.js
+++ script.js
@@ -4,16 +4,20 @@
 
 function dark(){
 
-//   var hour = (new Date).getHours();
-
-//   if (hour >= 14) {
-//     document.body.classList.toggle('dark');
-//   } else {
-//     document.body.classList.toggle('dark');
-//   }
-//   console.log(hour);
-
-// };
-    document.body.classList.toggle('dark')
-    
+  var today = new Date();
+  var time = today.getHours()
+  console.log(time);
+  
+  if (time >= 18) {
+    document.body.classList.toggle('dark');
+} if (time >= 6 && time <18){
+  document.body.remove.classList.toggle('dark');
 }
+
+
+};
+
+dark();
+//     document.body.classList.toggle('dark')
+    
+// }
