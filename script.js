22268a1cd9d413ee3b2d4ea5f35185e588754e22
ssambinelli <ssambinelli@icloud.com>
2023-04-26 16:44:32 -0400

    dark mode add

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.querySelector('.photo').addEventListener('click', () =>{
+//     document.body.classList.toggle('dark')
+// })
+
+function dark(){
+
+//   var hour = (new Date).getHours();
+
+//   if (hour >= 14) {
+//     document.body.classList.toggle('dark');
+//   } else {
+//     document.body.classList.toggle('dark');
+//   }
+//   console.log(hour);
+
+// };
+    document.body.classList.toggle('dark')
+    
+}
