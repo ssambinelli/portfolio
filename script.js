cdc6be2f24a966a17bff349ceaaa4ae18c542ac8
ssambinelli <ssambinelli@icloud.com>
2023-04-27 15:00:43 -0400

    fixed toggle button

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-// document.querySelector('.photo').addEventListener('click', () =>{
-//     document.body.classList.toggle('dark')
-// })
 
 function dark(){
-
-  var today = new Date();
-  // var time = today.getHours()
-  var time=19
-  
-  if (time >= 18) {
-    document.body.classList.toggle('dark');
-
-} else{
-  document.body.remove.classList.toggle('dark');
-}
-
-
+  document.body.classList.toggle('dark');
 };
 
-dark();
-//     document.body.classList.toggle('dark')
-    
-// }
+(function(){
+  var today = new Date();
+  var time = today.getHours()
+  if (time >= 18) {
+    var button = document.querySelector('.switch input')
+    button.click();
+  }
+})()
