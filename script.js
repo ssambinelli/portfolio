0e61d2628257b36a0b89f0b36a36fc7bb03aae2d
ssambinelli <ssambinelli@icloud.com>
2023-04-27 17:51:50 -0400

    responsive

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ function dark(){
 (function(){
   var today = new Date();
   var time = today.getHours()
+  // var time=20
   if (time >= 18) {
     var button = document.querySelector('.switch input')
     button.click();
   }
 })()
+
+
+function response(){
+  const burgerBtn = document.getElementsByClassName(".burger-btn")[0]
+  const navLinks = document.getElementsByClassName('.navbarLink')[0]
+  navLinks.classList.toggle('active')
+}
+
+// const burgerBtn = document.getElementsByClassName(".burger-btn")[0]
+// const navLinks = document.getElementsByClassName('.navbarLink')[0]
+
+// burgerBtn.addEventListener('click', ()=> {
+//   navLinks.classList.toggle('active')
+// })
